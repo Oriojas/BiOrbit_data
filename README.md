12d5df72ee9e08408933919e018c58bea27462c7
Oriojas <oscarriojas@gmail.com>
2023-02-21 22:07:00 -0500

    feat: include download images from sentinel

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BiOrbit_data
+<https://geoinnova.org/blog-territorio/imagenes-satelitales-gratuitas/>
+## API images:
+<https://docs.sentinel-hub.com/api/latest/reference/#tag/process>
+## Token Outh
+`curl --request POST --url https://services.sentinel-hub.com/oauth/token --header "content-type: application/x-www-form-urlencoded" --data "grant_type=client_credentials&client_id=<your client id>" --data-urlencode "client_secret=<your client secret>"`
---
+++ download_sat_img.py
@@ -0,0 +1,46 @@
+import os
+import requests
+
+TOKEN = os.getenv("TOKEN")
+
+response = requests.post('https://services.sentinel-hub.com/api/v1/process',
+  headers={"Authorization": f"Bearer {TOKEN}"},
+  json={
+    "input": {
+        "bounds": {
+            "bbox": [
+                13.822174072265625,
+                45.85080395917834,
+                14.55963134765625,
+                46.29191774991382
+            ]
+        },
+        "data": [{
+            "type": "sentinel-2-l2a"
+        }]
+    },
+    "evalscript": """
+    //VERSION=3
+
+    function setup() {
+      return {
+        input: ["B02", "B03", "B04"],
+        output: {
+          bands: 3
+        }
+      };
+    }
+
+    function evaluatePixel(
+      sample,
+      scenes,
+      inputMetadata,
+      customData,
+      outputMetadata
+    ) {
+      return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+    }
+    """
+})
+
+print(response)
