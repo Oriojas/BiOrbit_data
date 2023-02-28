477af58ec8f24c1f1952da91cc3d3de8fb9ba8e4
Oriojas <oscarriojas@gmail.com>
2023-02-27 21:27:54 -0500

    feat: normalize image ndvi

--- img_prepare.py
+++ img_prepare.py
@@ -3,6 +3,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 FILE = "img_input/2020-03-18-00_00_2020-03-18-23_59_Sentinel-2_L2A_True_color.png"
+NDVI_MIN = -1
+NDVI_MAX = 1
+
 
 coords = {"type": "Polygon",
           "coordinates": [[[-68.932746, 2.114557],
@@ -19,5 +22,15 @@ array_img = np.asarray(raw_img.getdata(), dtype=np.float64).reshape((raw_img.siz
 array_img = np.asarray(array_img, dtype=np.int16)
 img = Image.fromarray(array_img)
 
-plt.imshow(np.asarray(org_img))
+ndvi_data = ((((NDVI_MAX - NDVI_MIN) / 255) * array_img) - 1)
+
+ndvi_resume = {"min": ndvi_data.min(),
+               "max": ndvi_data.max(),
+               "mean": ndvi_data.mean()}
+
+hist_data = ndvi_data.flatten()
+
+plt.boxplot(hist_data)
 plt.show()
+#plt.imshow(np.asarray(org_img))
+#plt.show()
