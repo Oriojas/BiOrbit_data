11cfa8b20dffbad04c34fc3ef147c4df7076a9b3
Oriojas <oscarriojas@gmail.com>
2023-03-20 12:10:06 -0500

    fix: na in matrix img input

--- img_prepare.py
+++ img_prepare.py
@@ -2,6 +2,7 @@ import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class imgPrepare:
 
     def __init__(self, file_path, ndvi_min=-1, ndvi_max=1):
@@ -9,28 +10,36 @@ class imgPrepare:
         self.ndvi_max = ndvi_max
         self.file_path = file_path
 
+    @property
     def prepare(self):
         with rasterio.open(f"{self.file_path}") as src:
             org_img = src.read()
 
-        img_plot = np.array(org_img[4])
+        deforestation = np.array(org_img[4])
 
-        ndvi_data = ((((self.ndvi_max - self.ndvi_min) / 255) * org_img) - 1)
+        # ndvi_data = ((((self.ndvi_max - self.ndvi_min) / 255) * org_img) - 1)
 
         forest_data = np.zeros((org_img.shape[1], org_img.shape[2]), dtype=np.int8)
 
         for i in range(org_img.shape[1]):
             for j in range(org_img.shape[2]):
-                if img_plot[i, j] >= 0.7:
+                if np.isnan(deforestation[i, j]):
+                    deforestation[i, j] = 0
+                elif deforestation[i, j] > 1:
+                    deforestation[i, j] = 1
+                elif deforestation[i, j] >= 0.7:
                     forest_data[i, j] = 1
 
-        plt.imshow(forest_data, cmap='hot', interpolation='nearest')
-        plt.show()
+        # plt.imshow(forest_data, cmap='hot', interpolation='nearest')
+        # plt.show()
+        area_pixels = deforestation.shape[0] * deforestation.shape[1]
+        area_forest = forest_data.sum()
 
-        ndvi_resume = {"min": ndvi_data.min(),
-                       "max": ndvi_data.max(),
-                       "mean": ndvi_data.mean()}
+        resume = {"mean": deforestation.mean(),
+                  "total_px": area_pixels,
+                  "deforestation": area_pixels - area_forest,
+                  "percentage": round((1 - (area_forest / area_pixels)) * 100, 2)}
 
-        hist_data = ndvi_data.flatten()
+        hist_data = deforestation.flatten()
 
-        return ndvi_resume, hist_data, ndvi_data
+        return resume, hist_data, deforestation
