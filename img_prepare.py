60a09be1de89d621c5c03a09f0cbecdc7457ccc3
Oriojas <oscarriojas@gmail.com>
2023-03-19 20:30:29 -0500

    fix: compresed image canal

--- img_prepare.py
+++ img_prepare.py
@@ -1,5 +1,6 @@
 import rasterio
 import numpy as np
+import matplotlib.pyplot as plt
 
 class imgPrepare:
 
@@ -11,14 +12,20 @@ class imgPrepare:
     def prepare(self):
         with rasterio.open(f"{self.file_path}") as src:
             org_img = src.read()
-        raw_img = org_img.convert('L')
-        array_img = np.asarray(raw_img.getdata(), dtype=np.float64).reshape((raw_img.size[1],
-                                                                             raw_img.size[0]))
 
-        array_img = np.asarray(array_img, dtype=np.int16)
-        img = Image.fromarray(array_img)
+        img_plot = np.array(org_img[4])
 
-        ndvi_data = ((((self.ndvi_max - self.ndvi_min) / 255) * array_img) - 1)
+        ndvi_data = ((((self.ndvi_max - self.ndvi_min) / 255) * org_img) - 1)
+
+        forest_data = np.zeros((org_img.shape[1], org_img.shape[2]), dtype=np.int8)
+
+        for i in range(org_img.shape[1]):
+            for j in range(org_img.shape[2]):
+                if img_plot[i, j] >= 0.7:
+                    forest_data[i, j] = 1
+
+        plt.imshow(forest_data, cmap='hot', interpolation='nearest')
+        plt.show()
 
         ndvi_resume = {"min": ndvi_data.min(),
                        "max": ndvi_data.max(),
