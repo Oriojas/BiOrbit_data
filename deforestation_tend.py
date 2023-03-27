2a6f71a21cdcf8f0f9ec80a664b38a1afe099f8b
Oriojas <oscarriojas@gmail.com>
2023-03-26 22:47:10 -0500

    feat: create deforestation_tend and test class

--- deforestation_tend.py
+++ deforestation_tend.py
@@ -7,83 +7,99 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.metrics import r2_score
 
-with open('/home/oscar/GitHub/BiOrbit_data/img_data/data.json', 'r') as f:
-    data = json.load(f)
 
-dict_df = {"date": data["detection_date_list"],
-           "deforestation": data["total_extension_forest_cover_list"]}
+class DataProcess:
 
-df = pd.DataFrame.from_dict(dict_df)
+    def __init__(self, file_meta: str, ndvi_tif: str):
 
-df["date"] = df["date"].apply(lambda x: x[0:10])
+        self.file_meta = file_meta
+        self.ndvi_tif = ndvi_tif
 
-df["date"] = pd.to_datetime(df["date"])
+    def process_meta(self, reg_date: str, view_plot: bool = False, log: bool = False, plot_name: str = "plot_tend"):
 
-X_train = np.array(df["date"].apply(lambda x: pd.to_datetime(x).timestamp()))
-X_train = X_train.reshape(-1, 1)
+        with open(self.file_meta, 'r') as f:
+            m_data = json.load(f)
 
-y_train = np.array(df["deforestation"])
+        dict_df = {"date": m_data["detection_date_list"],
+                   "deforestation": m_data["total_extension_forest_cover_list"]}
 
-regr = linear_model.LinearRegression()
+        df_meta = pd.DataFrame.from_dict(dict_df)
+        df_meta["date"] = df_meta["date"].apply(lambda x: x[0:10])
+        df_meta["date"] = pd.to_datetime(df_meta["date"])
+        x_train = np.array(df_meta["date"].apply(lambda x: pd.to_datetime(x).timestamp()))
+        x_train = x_train.reshape(-1, 1)
 
-regr.fit(X_train, y_train)
+        y_train = np.array(df_meta["deforestation"])
 
-y_pred = regr.predict(X_train)
+        regr = linear_model.LinearRegression()
+        regr.fit(x_train, y_train)
+        y_pred = regr.predict(x_train)
+        df_meta["pred"] = y_pred
 
-df["pred"] = y_pred
+        dict_pre = {"date": pd.to_datetime(reg_date),
+                    "deforestation": regr.predict(np.array(pd.to_datetime(reg_date).timestamp()).reshape(-1, 1)),
+                    "pred": regr.predict(np.array(pd.to_datetime(reg_date).timestamp()).reshape(-1, 1))}
 
-dict_pre = {"date": pd.to_datetime("2023-05-30"),
-            "deforestation": regr.predict(np.array(pd.to_datetime("2023-05-30").timestamp()).reshape(-1, 1)),
-            "pred": regr.predict(np.array(pd.to_datetime("2023-05-30").timestamp()).reshape(-1, 1))}
+        df_meta_pred = pd.DataFrame.from_dict(dict_pre)
+        df_meta = pd.concat([df_meta, df_meta_pred])
 
-df_pred = pd.DataFrame.from_dict(dict_pre)
+        plt.plot(df_meta["date"].iloc[0:-1], df_meta["deforestation"].iloc[0:-1], "-o", label="deforestation")
+        plt.plot(df_meta["date"], df_meta["pred"], "--", label="projection")
+        plt.hlines(y=m_data["total_extension_protected_area"],
+                   xmin=df_meta["date"].min(), xmax=df_meta["date"].max(),
+                   label="total_area", color="green")
 
-df = pd.concat([df, df_pred])
+        plt.title(f"Deforestation: {m_data['protected_area_name']},"
+                  f"R2={round(r2_score(df_meta['deforestation'], df_meta['pred']), 2)}")
+        plt.legend(loc='lower right')
+        if log:
+            plt.yscale("log")
+        plt.grid()
+        if view_plot:
+            plt.show()
+        plt.savefig(f"/home/oscar/GitHub/BiOrbit_data/plots/{plot_name}_boxp.png")
 
-# plt.plot(df["date"].iloc[0:-1], df["deforestation"].iloc[0:-1], "-o", label="deforestation")
-# plt.plot(df["date"], df["pred"], "--", label="projection")
-# plt.hlines(y=data["total_extension_protected_area"],
-#            xmin=df["date"].min(), xmax=df["date"].max(),
-#            label="total_area", color="green")
-#
-# plt.title(f"Deforestation: {data['protected_area_name']}, R2={round(r2_score(df['deforestation'], df['pred']), 2)}")
-# plt.legend(loc='lower right')
-# # plt.yscale("log")
-# plt.grid()
-# plt.show()
+        plt.boxplot(df_meta["deforestation"])
+        if view_plot:
+            plt.show()
+        plt.savefig(f"/home/oscar/GitHub/BiOrbit_data/plots/{plot_name}_reg.png")
 
-FILE = "/home/oscar/GitHub/BiOrbit_data/img_input/2023-03-14-LC09_B2_B3_B4_B5_multiband_NDVI_masked_added.TIF"
+        print("Regression OK")
 
-ipre_obj = ipre.ImgPrepare(file_path=FILE)
+    def process_ndvi_tif(self, view_plot: bool = False):
 
-_, _, data = ipre_obj.prepare()
+        ipre_obj = ipre.ImgPrepare(file_path=self.ndvi_tif)
+        _, _, data = ipre_obj.prepare()
 
-# plt.boxplot(df["deforestation"])
-# plt.show()
+        rgb_image = ipre_obj.rgb()
+        plt.imshow(data, cmap='hot', interpolation='nearest')
+        plt.axis("off")
+        plt.savefig("/home/oscar/GitHub/BiOrbit_data/img_output/Deforestation.png",
+                    bbox_inches='tight', pad_inches=0)
+        if view_plot:
+            plt.show()
 
-rgb_image = ipre_obj.rgb()
+        plt.imshow(rgb_image)
+        plt.axis("off")
+        plt.savefig("/home/oscar/GitHub/BiOrbit_data/img_output/RGB_image.png",
+                    bbox_inches='tight', pad_inches=0)
+        if view_plot:
+            plt.show()
 
-plt.imshow(data, cmap='hot', interpolation='nearest')
-plt.axis("off")
-plt.savefig("/home/oscar/GitHub/BiOrbit_data/img_output/Deforestation.png",
-            bbox_inches='tight', pad_inches=0)
-# plt.show()
+        img_def = cv2.imread('/home/oscar/GitHub/BiOrbit_data/img_output/Deforestation.png',
+                             cv2.IMREAD_UNCHANGED)
+        img_rgb = cv2.imread("/home/oscar/GitHub/BiOrbit_data/img_output/RGB_image.png",
+                             cv2.IMREAD_UNCHANGED)
 
-plt.imshow(rgb_image)
-plt.axis("off")
-plt.savefig("/home/oscar/GitHub/BiOrbit_data/img_output/RGB_image.png",
-            bbox_inches='tight', pad_inches=0)
-# plt.show()
+        final_img = cv2.addWeighted(img_def, 0.2, img_rgb, 1, 0)
 
-img_def = cv2.imread('/home/oscar/GitHub/BiOrbit_data/img_output/Deforestation.png',
-                     cv2.IMREAD_UNCHANGED)
-img_rgb = cv2.imread("/home/oscar/GitHub/BiOrbit_data/img_output/RGB_image.png",
-                     cv2.IMREAD_UNCHANGED)
+        cv2.imwrite('/home/oscar/GitHub/BiOrbit_data/img_output/final_img.png',
+                    final_img)
 
-final_img = cv2.addWeighted(img_def, 0.2, img_rgb, 1, 0)
+        if view_plot:
+            print("Press 0 for close window")
+            cv2.imshow('final_img', final_img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-cv2.imwrite('/home/oscar/GitHub/BiOrbit_data/img_output/final_img.png', final_img)
-cv2.imshow('final_img', final_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+        print("Process img OK")
